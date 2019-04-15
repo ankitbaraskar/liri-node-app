6d300377245cf879a8446e5e7b1ea8221622eaca
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-04-14 23:03:06 -0400

    added spotify function

--- liri.js
+++ liri.js
@@ -24,9 +24,9 @@ switch (command) {
     case "spotify-this-song":
         spotifyCall();
         break;
-    // case "withdraw":
-    //     withdraw(amount);
-    //     break;
+    case "movie-this":
+        movieCall();
+        break;
     // case "lotto":
     //     lotto();
     //     break;
@@ -41,28 +41,67 @@ function concert() {
     console.log(url);
 
     axios.get(url).then(function (response) {
-       var data = response.data;
-    //    console.log(response);
+        var data = response.data;
+        //    console.log(response);
         // console.log(data[0]);
 
         data.forEach(element => {
             var result = {};
             result.VenueName = element.venue.name;
-            result.Location = element.venue.city+', '+element.venue.country;
+            result.Location = element.venue.city + ', ' + element.venue.country;
 
-            var convertedDate = moment(element.datetime,"YYYY-MM-DD")
+            var convertedDate = moment(element.datetime, "YYYY-MM-DD")
             result.Date = convertedDate.format("MM/DD/YYYY");
             console.table(result);
         });
 
     })
         .catch(function (error) {
-            
+
             console.log(error);
         });
 }
 
 
-function spotifyCall (){
+function spotifyCall() {
+
+    var songNameRequest = searchParameters.join(' ');
+
+    if (songNameRequest) {
+        var Songurl = "https://api.spotify.com/v1/search?q=" + songNameRequest + "&type=track&limit=1"
+    }
+    else {
+        var Songurl = "https://api.spotify.com/v1/search?q=The Sign Ace of Base&type=track&limit=1"
+    }
+
+    spotify.request(Songurl).then(function (response) {
+        var artistname = response.tracks.items[0].album.artists[0].name;
+        var songName = response.tracks.items[0].name;
+        var previewLink = response.tracks.items[0].preview_url;
+        var albumName = response.tracks.items[0].album.name;
+
+        var result = {};
+        result.Artist = artistname;
+        result.Song = songName;
+        result.Preview_URL = previewLink;
+        result.Album = albumName;
+        console.table(result);
+        // console.log(response.tracks.items[0].preview_url);
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
+
+function movieCall(){
     
 }
